0344f1223303ca491d8876049dac727d8a741b90
dawid <dawidmantojfel@gmail.com>
2021-01-10 09:55:05 +0100

    Created requirements.txt

--- Market/admin.py
+++ Market/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Users.models import User
+from Market.models import Book, Market, Opinion
+
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Market)
+admin.site.register(User)
+admin.site.register(Opinion)
--- Market/models.py
+++ Market/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from Users.models import User
+# Create your models here.
+
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    rate = models.FloatField(null=True)
+
+
+class Market(models.Model):
+    CONDITION_CHOICES = (
+        ('bad', 'bad'),
+        ('used', 'used'),
+        ('new', 'new'),
+    )
+    ''' user powinien byc many to many field z ksiazka
+    bo uzytkownik moze miec wiele ksiazek  i jedna
+    ksiazka moze miec wielu uzytkownikow '''
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    price = models.FloatField()
+    sold = models.BooleanField(default=False)
+    condition = models.CharField(max_length=4, choices=CONDITION_CHOICES)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Opinion(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    opinion = models.CharField(max_length=2000)
